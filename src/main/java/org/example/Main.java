0b0271e2917345ac86a251f91324d6c791119c61
Bora Ciner <bora.ciner@audivo.com>
2023-01-07 14:25:32 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    static Queue<Integer> dataQueue = new LinkedList();
+    static ReadWriteLock readWriteLock = new ReentrantReadWriteLock(true);
+
+
+    public static void main(String[] args) {
+
+        Thread producer = new Thread(new Producer());
+        producer.start();
+
+        for (int i = 0; i < 100; i++) {
+            Thread consumer = new Thread(new Consumer());
+            consumer.setName("Consumer-"+i);
+            consumer.start();
+        }
+    }
+
+    public static class Producer implements Runnable{
+
+        @Override
+        public void run() {
+            while (true) {
+                readWriteLock.writeLock().lock();
+                if(dataQueue.size() < 10) {
+                    System.out.println("Produce Entered");
+                    dataQueue.offer(4);
+                    System.out.println("Produce Exited");
+
+                }
+                readWriteLock.writeLock().unlock();
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+
+    public static class Consumer implements Runnable{
+
+        @Override
+        public void run() {
+            while (true) {
+                readWriteLock.readLock().lock();
+                if(dataQueue.size() ==10) {
+                    dataQueue.poll();
+                    System.out.println("Consumer["+Thread.currentThread().getName()+"] taking... ( size "+dataQueue.size()+")");
+                }
+                readWriteLock.readLock().unlock();
+
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
